f6ee2db29c30d3e8a2d055f9498396a0f767e68f
lsw3210 <lsw3210@gmail.com>
2021-04-28 19:22:52 +0900

    first

--- flask_app.py
+++ flask_app.py
@@ -5,13 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template("hello.html")
-
-
-@app.route('/apply')
-def apply():
-    return render_template("apply.html")
-
-
-@app.route('/list')
-def list():
-    return render_template("list.html")
